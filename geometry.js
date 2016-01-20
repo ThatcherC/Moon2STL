098252ba605bb3a90aeaca2008fd7d830f7f0dc3
Thatcher Chamberlin <j.thatcher.c@gmail.com>
2016-01-20 17:38:43 -0500

    Added geometry functions

---
+++ geometry.js
@@ -0,0 +1,8 @@
+var d2r = Math.PI/180;
+
+function sphericalToCartesian(s){
+  var x = s.r*Math.sin(s.lat*d2r)*Math.cos(s.lng*d2r);
+  var y = s.r*Math.sin(s.lat*d2r)*Math.sin(s.lng*d2r);
+  var z = s.r*Math.sin(s.lat*d2r);
+  return {x:sphere.r*}
+}
