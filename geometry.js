4ded1f35bd8a248603c1742af89476a88c3b3a24
Thatcher Chamberlin <j.thatcher.c@gmail.com>
2016-01-20 17:38:58 -0500

    Added geometry functions

--- geometry.js
+++ geometry.js
@@ -4,5 +4,5 @@ function sphericalToCartesian(s){
   var x = s.r*Math.sin(s.lat*d2r)*Math.cos(s.lng*d2r);
   var y = s.r*Math.sin(s.lat*d2r)*Math.sin(s.lng*d2r);
   var z = s.r*Math.sin(s.lat*d2r);
-  return {x:sphere.r*}
+  return {x:x,y:y,z:z};
 }
