6a1669d4d7693c38784ea96e943110765be9790b
Thatcher Chamberlin <j.thatcher.c@gmail.com>
2016-01-21 14:04:46 -0500

    Added exports and test code

--- elevationgetter.js
+++ elevationgetter.js
@@ -1,15 +1,32 @@
-//test reference function
-var t = 0;
-var delta = vectorSubtract(sphericalToCartesian(se),sphericalToCartesian(sw));
-var inc = vectorMul(delta,1/40);
+var geometry = require("./geometry");
 
-function advanceMarker(increment){
-  marker = new google.maps.Marker({
-    position: cartesianToSpherical(vectorAdd(sphericalToCartesian(myLatLng), vectorMul(increment,t))),
-    map: map,
-    title: 'Hello World!'
-  });
-  console.log(cartesianToSpherical(vectorAdd(sphericalToCartesian(myLatLng), vectorMul(increment,t))));
-  t++;
-  t=t%40;
+function getElevations(se,sw,nw,image,callback){
+  var width = 40;
+  var height = 40;
+
+  var incx = vectorMul(vectorSubtract(sphericalToCartesian(se),sphericalToCartesian(sw)),1/width);
+  var incy = vectorMul(vectorSubtract(sphericalToCartesian(nw),sphericalToCartesian(sw)),1/height);
+  var start = sphericalToCartesian(sw);
+
+  var elevations;
+
+  for(var y = 0; y <height; y++){
+    for(var x = 0; x < width; x++){
+      var position = cartesianToSpherical(vectorAdd(start, vectorAdd(vectorMul(incx,x),vectorMul(incy,y))));
+
+      //geotiff has resolution of 16px per degree
+      //this info can be procedurally gathered from geotiff in the future
+      //Using floor is just a quick way to test this - interpolation would be better
+      position.lat = Math.floor((90-position.lat)*16);
+      position.lng = Math.floor((position.lng+180)*16);
+
+      console.log(image.readRasters([position.lat,position.lng+1,position.lat+1,position.lng]));
+      //elevations[y*width+x] = image.readRasters([position.lat,position.lng+1,position.lat+1,position.lng]);
+    }
+  }
+
 }
+
+module.exports = {
+  getElevations: getElevations
+};
