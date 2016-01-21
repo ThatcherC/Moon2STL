e5e4b0273a55224539d8b460fbd1f7ce751bb756
Thatcher Chamberlin <j.thatcher.c@gmail.com>
2016-01-21 14:04:19 -0500

    Changed method to POST and added code to accept parameters

--- moonstlserver.js
+++ moonstlserver.js
@@ -1,12 +1,14 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var Concentrate = require("concentrate");
 var GeoTIFF = require("geotiff");
 var fs = require("fs");
 
 var stlStreamer = require("./stlstreamer");
-var stlStreamer = require("./geometry");
 
 var app = express();
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
 var path = "ulcn2005_lpo_dd0.tif";
 var moontiff;
@@ -15,16 +17,15 @@ fs.readFile(path, function(err, data) {
   if (err) throw err;
   moontiff = GeoTIFF.parse(data);
   image = moontiff.getImage();
-  console.log(moontiff.fileDirectories[0][0]);
-  console.log(moontiff.fileDirectories[0][1]);
-  //console.log(moontiff.getImage().readRasters([1000,1000,1100,1010]));
+  //console.log(moontiff.fileDirectories[0][0]);
+  //console.log(moontiff.fileDirectories[0][1]);
 });
 
 
 var count = 0;
 
-app.get("/stl",function(req,res){
-  console.time(count);
+app.post("/stl",function(req,res){
+  //console.time(count);
   var c = Concentrate();
   c.on("end", function() {
     res.end();
@@ -32,22 +33,29 @@ app.get("/stl",function(req,res){
     c.pipe(res);
   });
 
-  res.setHeader('Content-disposition', 'attachment; filename=' + req.query.name);
+  res.setHeader('Content-disposition', 'attachment; filename=' + req.body.name);
   res.setHeader('Content-type', "application/sla");
   res.writeHead(200);
 
-  var rasterWindow = [req.query.l,req.query.t,req.query.r,req.query.b];
+  var width = req.body.width;
+  var height = req.body.height;
+  var sw = {lat:Number(req.body.swlat), lng:Number(req.body.swlng)};
+  var se = {lat:Number(req.body.selat), lng:Number(req.body.selng)};
+  var nw = {lat:Number(req.body.nwlat), lng:Number(req.body.nwlng)};
 
-  var width = rasterWindow[2]-rasterWindow[0];
-  var height = rasterWindow[3]-rasterWindow[1];;
+  console.log(sw);
+  console.log(nw);
+  console.log(se);
 
+
+  /*
   elevationData = {xlen:width, ylen:height, scale:1/100, values: image.readRasters(rasterWindow)[0]};
 
   stlStreamer.stream(elevationData,c,function(){
     c.flush().end();
     console.timeEnd(count);
     count++;
-  });
+  });*/
 });
 
 app.listen(9000);
