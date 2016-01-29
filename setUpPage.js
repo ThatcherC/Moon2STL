722646fcea721eeee1c4e499d385b2c97f04295a
ThatcherC <j.thatcher.c@gmail.com>
2016-01-29 20:36:44 +0000

    Hid my Google Maps API key and added a way to add an API key easily

---
+++ setUpPage.js
@@ -0,0 +1,24 @@
+//Made with help from https://nodejs.org/api/readline.html
+//and https://www.npmjs.com/package/replace
+
+var replace = require("replace");
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question('Enter a valid Google Maps API key: ', function(apikey){
+  // TODO: Log the answer in a database
+  console.log('API Key: ', apikey);
+
+  replace({
+    regex: "YOURGMAPSAPIKEY",
+    replacement: apikey,
+    paths: ['./Moon2STL.html']
+  });
+
+  rl.close();
+});
+
