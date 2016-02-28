b30d7553bb812887839c613b6c1d1e731ebe563d
Thatcher Chamberlin <j.thatcher.c@gmail.com>
2016-02-27 21:49:13 -0500

    Added STL test code

--- gtelevstlsrc/vectorTest.cpp
+++ gtelevstlsrc/vectorTest.cpp
@@ -1,10 +1,22 @@
 #include <iostream>
 #include "Vector.h"
+#include "LatLng.h"
+#include "STLWriter.h"
+#include <vector>
 
 int main(){
   int a = 2;
-  Vector v(1,2.0,3);
+  Vector v(-1,0,1);
   Vector v2(3,2,1);
   Vector v3 = v.multiply(3);
   v3.print();
+  LatLng l = v.toSpherical();
+  l.print();
+  std::vector<float> d;
+  d.push_back(.5);
+  d.push_back(.5);
+  d.push_back(.6);
+  d.push_back(.7);
+
+  writeSTLfromArray(d, 2, 2);
 }
